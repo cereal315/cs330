edf6957e0568e070455e4fff16e4d96732d765e7
cereal315 <sanicabaca@gmail.com>
2022-12-02 19:22:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// const fName = document.getElementbyID("f-Name");
+// const email = document.getElementbyID("email");
+// const form = document.getElementbyID("subForm");
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+// })
+
+// function validateSubForm(){
+//     // if the user hits subscribe button w/o entering
+//     //  first name and email, will not accept
+//     if (fName == "") {
+//         return false;
+//     }
+// }
